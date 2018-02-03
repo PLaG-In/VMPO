0178030876274709b80f96c9ef16365eb8b5f083
Vladimir A <vladimir.alt@inbox.ru>
2018-02-03 15:38:50 +0300

    Нормальный ход времени
    :recycle:

--- js/timer.js
+++ js/timer.js
@@ -8,22 +8,18 @@ function startTimer() {
 	var h = arr[0];
 	var m = arr[1];
 	var s = arr[2];
-	if (s == 0) {
-		if (m == 0) {
-			if (h == 0) {
-			  alert("Время вышло");
-			  //window.location.reload();
-			  return;
-			}
-			h--;
-			m = 60;
+	if (s == 59) {
+		if (m == 59) 
+		{
+			h++;
+			m = 0;
 			if (h < 10) h = "0" + h;
 		}
-		m--;
+		else m++;
 		if (m < 10) m = "0" + m;
-		s = 59;
+		s = 0;
 	}
-	else s--;
+	else s++;
 	if (s < 10) s = "0" + s;
 	document.getElementById("time").innerHTML = h+":"+m+":"+s;
 	setTimeout(startTimer, 1000);
