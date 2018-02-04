0cf9a0da2f5acf0021a8144cbb6586f9fa862fda
psmax <shmarov_max@mail.ru>
2018-02-04 21:16:07 +0300

    Added popup

--- js/hidePopup.js
+++ js/hidePopup.js
@@ -1,6 +1,8 @@
 		// Get the modal
 var signup = document.getElementById('id01');
 var login = document.getElementById('id02');
+var add = document.getElementById('id03');
+var edit = document.getElementById('id04');
 
 	// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -8,5 +10,10 @@ window.onclick = function(event) {
 		signup.style.display = "none";
 	} else if (event.target == login){
 		login.style.display = "none";
+	} else if(event.target == add){
+		add.style.display = "none";
+	} else	if(event.target == edit){
+		edit.style.display = "none"
 	}
+
 }
